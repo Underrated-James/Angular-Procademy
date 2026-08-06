08050228833818587a036df75ca83f0929136abe
johnpaul_m <johnpaul_m@cyber-brothers.com>
2026-08-06 10:25:24 +0800

    styling view tremplate episode 9 ekart

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,25 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { routes } from './app.routes';
+
+//added
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.my-app-dark',
+        }
+      }
+    })
   ]
 };
