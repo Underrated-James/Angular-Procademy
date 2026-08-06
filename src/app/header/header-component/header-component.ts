c5f2b46023c7cf17210c252f081fd4426956793b
johnpaul_m <johnpaul_m@cyber-brothers.com>
2026-08-06 13:31:53 +0800

    Adding CSS Styles Globally

--- src/app/header/header-component/header-component.ts
+++ src/app/header/header-component/header-component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-header-component',
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './header-component.html',
   styleUrl: './header-component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  
+}
