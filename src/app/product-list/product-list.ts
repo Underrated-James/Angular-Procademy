b0d2e11bd43fd54d0f051aa90a1094733ba88e8b
johnpaul_m <johnpaul_m@cyber-brothers.com>
2026-08-12 13:32:20 +0800

    Property Binding | Angular Components & Directives

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -1,22 +1,37 @@
 import { Component, computed } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { AutoCompleteCompleteEvent, AutoCompleteModule } from 'primeng/autocomplete';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [Button, AutoCompleteModule, FormsModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
 export class ProductList {
+
+  items: any[] = [];
+  value: any;
 
   product = {
     name: 'iPhone',
     price: 999,
     discountedName: 'Samsung Galaxy',
     discountedPrice: 899,
-    inStock: 0,
-    seller: "Apple"
+    inStock: 5,
+    seller: "Apple",
+    img: '/Iphone 17.jpeg'
   }
+
+  isDisabled = computed(() => {
+    if(this.product.inStock > 0){
+      return false;
+    } else {
+      return true;
+    }
+  })
+
 
   fallback: number = 599;
   
@@ -24,5 +39,13 @@ export class ProductList {
     return this.product.discountedPrice / 3 || this.fallback;
   })
   
+
+  addToCart(){
+    console.log('Button clicked! Adding product to cart...');
+  }
+  
+  search(event: AutoCompleteCompleteEvent) {
+    this.items = [...Array(10).keys()].map((item) => event.query + '-' + item);
+  }
 
 }
