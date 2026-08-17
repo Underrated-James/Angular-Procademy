db4abd8a34bcc39411107d092e5841d931919ec1
johnpaul_m <johnpaul_m@cyber-brothers.com>
2026-08-17 14:29:11 +0800

    MODERN ngFor Directive | Angular Components & Directives

--- src/app/product-list/product-list.ts
+++ src/app/product-list/product-list.ts
@@ -1,11 +1,12 @@
 import { Component, computed, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { Search } from './search/search';
 
 @Component({
   selector: 'app-product-list',
-  imports: [FormsModule, InputTextModule, Search],
+  imports: [FormsModule, InputTextModule, Search, ButtonModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
@@ -16,7 +17,9 @@ export class ProductList {
   fallback: number = 599;
   qty = signal(0);
 
-  product = {
+  products = [
+    {
+    id: 1,
     name: 'iPhone',
     price: 999,
     discountedName: 'Samsung Galaxy',
@@ -25,13 +28,14 @@ export class ProductList {
     seller: "Apple",
     img: '/Iphone 17.jpeg'
   }
+]
 
   dynamicPrice = computed(() => {
-    return this.product.price * this.qty() || 0;
-  })
+return (this.products[0]?.price || 0) * this.qty();
+})
 
   isDisabled = computed(() => {
-    if(this.product.inStock > 0){
+    if(this.products[0]?.inStock > 0){
       return false;
     } else {
       return true;
@@ -39,7 +43,7 @@ export class ProductList {
   })
   
   getDiscountedPrice = computed(() => {
-    return this.product.discountedPrice / 3 || this.fallback;
+    return (this.products[0]?.discountedPrice || 0) / 3 || this.fallback;
   })
   
   addToCart(){
